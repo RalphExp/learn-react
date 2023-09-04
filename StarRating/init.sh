eec3270b4151517eb6116a77e31a442a7c80f66f
ralph <ralphxwz@gmail.com>
2023-09-04 21:58:58 +0800

    update

---
+++ StarRating/init.sh
@@ -0,0 +1,5 @@
+npm init -y
+npm install react react-dom react-icons serve
+npm install --save-dev webpack webpack-cli
+npm install --save-dev babel-loader 
+npm install --save-dev @babel/core  @babel/preset-env @babel/preset-react
