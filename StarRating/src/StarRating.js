5ec367c456644c067276800be442682dc8bfa06c
ralph <ralphxwz@gmail.com>
2023-09-08 21:46:23 +0800

    update

--- StarRating/src/StarRating.js
+++ StarRating/src/StarRating.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { createArray } from "./lib";
 import Star from "./Star";
 
-export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
-  return (
-    <>
-      {createArray(totalStars).map((n, i) => (
-        <Star key={i} selected={selectedStars > i} />
-      ))}
-      <p>
-        {selectedStars} of {totalStars} stars
-      </p>
-    </>
-  );
-}
+export default function StarRating({
+    totalStars = 5,
+    selectedStars = 0,
+    onRate = f => f
+  }) {
+    return (
+      <>
+        {createArray(totalStars).map((n, i) => (
+          <Star
+            key={i}
+            selected={selectedStars > i}
+            onSelect={() => onRate(i + 1)}
+          />
+        ))}
+        <p>
+          {selectedStars} of {totalStars} stars
+        </p>
+      </>
+    );
+  }
+  
