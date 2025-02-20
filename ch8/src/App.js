4a3f010cf90262563a90ef3f4a13d79f8933f6b8
RalphExp <ralphxwz@gmail.com>
2025-02-20 13:39:28 -0500

    github user example

---
+++ ch8/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { useState } from 'react';
+import GithubUser from './GithubUser';
+
+export default function App() {
+    console.log("loading app")
+    return (
+      <>
+        <GithubUser />
+      </>
+    );
+}
+
