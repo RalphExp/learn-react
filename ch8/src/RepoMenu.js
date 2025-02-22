f8349b66241951d61dbcc011656efea098836c39
RalphExp <ralphxwz@gmail.com>
2025-02-21 23:04:07 -0500

    fix Markdown

--- ch8/src/RepoMenu.js
+++ ch8/src/RepoMenu.js
@@ -1,23 +1,21 @@
 import React from "react";
 import { useEffect } from "react";
 import { useIterator } from "./hooks";
+import RepositoryReadme from "./RepositoryReadme";
 
-export default function RepoMenu({repositories, onSelect = f => f}) {
+export default function RepoMenu({repositories, login}) {
     const [{ name }, previous, next] = useIterator(
         repositories
     );
 
-    useEffect(() => {
-        if (!name)
-            return;
-        onSelect(name);
-    }, [name]);
-
     return (
-        <div style={{ display: "flex" }}>
-            <button onClick={previous}>&lt;</button>
-            <p>{name}</p>
-            <button onClick={next}>&gt;</button>
-        </div>
+        <>
+            <div style={{ display: "flex" }}>
+                <button onClick={previous}>&lt;</button>
+                <p>{name}</p>
+                <button onClick={next}>&gt;</button>
+            </div>
+            <RepositoryReadme login={login} repo={name} />
+        </>
     );
 }
